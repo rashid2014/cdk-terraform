491fa510e285a132bd2d521687156ccc5317c878
Harun <harun.r.metroc@gnail.com>
2025-02-11 21:51:57 -0500

    Add terraform Buildspec

--- provider.tf
+++ provider.tf
@@ -7,6 +7,12 @@ terraform {
   }
 
   required_version = ">= 1.2.0"
+
+  backend "s3" {
+    bucket         = "terraformstatefileharunfeb112025"  # Replace with your S3 bucket name
+    key            = "terraform.tfstate"          # State file location within the bucket
+    region         = "var.region"                  # AWS region
+  }
 }
 
 provider "aws" {
